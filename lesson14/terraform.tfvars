47ca686c39477263ea131658c29017d12ce86188
donttracked <97156681+donttracked@users.noreply.github.com>
2024-10-02 03:00:03 +0500

    урок 14 терраформ (#22)

---
+++ lesson14/terraform.tfvars
@@ -0,0 +1,4 @@
+yandex_token = ""
+yandex_cloud_id = "b1gvl2225noj5jh7jvr5"
+yandex_folder_id = "b1gvl2225noj5jh7jvr5"
+zone = "ru-central1-a"
---
+++ lesson14/variables.tf
@@ -0,0 +1,16 @@
+variable "yandex_token" {
+  description = "Yandex Cloud OAuth token"
+}
+
+variable "yandex_cloud_id" {
+  description = "Yandex Cloud ID"
+}
+
+variable "yandex_folder_id" {
+  description = "Yandex Folder ID"
+}
+
+variable "zone" {
+  description = "Availability Zone"
+  default     = "ru-central1-a"
+}
